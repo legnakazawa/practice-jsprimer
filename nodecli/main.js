41307c0f40aac1d4ade9416193d0da9d52052497
Kaito Nakazawa <rock.pop.dj.cronos@gmail.com>
2025-03-17 21:52:22 +0900

    add: Node.jsでCLIアプリハンズオン追加

---
+++ nodecli/main.js
@@ -0,0 +1,27 @@
+import * as util from "node:util"
+import * as fs from "node:fs/promises"
+import { md2html } from "./md2html";
+
+const { 
+  values,
+  positionals
+} = util.parseArgs({
+  allowPositionals: true,
+  options: {
+    gfm: {
+      type: "boolean",
+      default: false,
+    }
+  }
+})
+
+const filePath = positionals[0];
+fs.readFile(filePath, { encoding: "utf8"}).then(file => {
+  const html = md2html(file, {
+    gfm: values.gfm
+  })
+  console.log(html);
+}).catch(err => {
+  console.error(err.message)
+  process.exit(1)
+})
